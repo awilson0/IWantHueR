b4234edf9b1d5190e01e7be93529f12c8c271dfb
Andrew Wilson <andrew.wilson849@icloud.com>
2021-01-15 11:23:40 -0800

    chore: Cleans up unneeded files

--- data_raw/rgb_colours.R
+++
@@ -1,5 +0,0 @@
-full_rgb <- as.matrix(expand.grid(rep(list(0:255), 3)))/255
-full_Lab <- convertColor(full_rgb, "sRGB", "Lab")
-
-
-usethis::use_data(full_rgb, full_Lab, internal = TRUE, overwrite = TRUE)
