f4dce9ac58347ab6d4a6c13f55bbd3c1d734d451
Andrew Wilson <andrew.wilson849@icloud.com>
2021-01-15 00:03:32 -0800

    feat: Adds L*a*b* colours to sysdata

--- data_raw/rgb_colours.R
+++ data_raw/rgb_colours.R
@@ -1,2 +1,5 @@
 full_rgb <- as.matrix(expand.grid(rep(list(0:255), 3)))/255
-usethis::use_data(full_rgb, internal = TRUE)
+full_Lab <- convertColor(full_rgb, "sRGB", "Lab")
+
+
+usethis::use_data(full_rgb, full_Lab, internal = TRUE, overwrite = TRUE)
