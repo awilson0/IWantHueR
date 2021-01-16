90099f94d41d075ac5973c9a354b0a71b9f4a627
Andrew Wilson <andrew.wilson849@icloud.com>
2021-01-15 23:35:45 -0800

    feat: Converts linear RGB to sRGB

--- R/colour_tools.R
+++ R/colour_tools.R
@@ -1,13 +1,12 @@
-#' create_lab
-#'
-#' create_lab takes a subset RGB colours and converts it to CIE L*a*b* format
-#'
-#' @param rgb A matrix of colours in RGB format.
-#' @export
+#' xyz_to_rgb
+#' xyz_to_rgb corrects for gamma to convert linear RGB to sRGB
+#' @param u The intensity of one component (R, G, or B) of a colour defined in RGB
 
-create_lab <- function(rgb = "full"){
-  if(rgb == "full"){
-    colours = IWantHueR:::full_rgb
+xyz_to_rgb <- function(u){
+  if(u <= 0.00304){
+    return(round(255 * 12.92 * u))
+  } else {
+    return(round(255 * (1.055 * u^(1/2.4) - 0.055)))
   }
-  convertColor(colours, "sRGB", "Lab")
 }
+
---
+++ tests/testthat/colour_tools_tests.R
@@ -0,0 +1,9 @@
+# xyz_to_rgb
+test_that("IWantHueR:::xyz_to_rgb() uses the correct functions for gamma correction",{
+  r <- 0.2000
+  g <- 0.0002
+  b <- 0
+
+  rgb <- cbind(r,g,b)
+  testthat::expect_equal(sapply(rgb, function(x){xyz_to_rgb(x)}), c(124, 1, 0))
+})
